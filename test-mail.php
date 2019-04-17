4705b6eb59fac0ef40ae7f33d14c9f3f082dd4ac
Daniel Colangelo <Danalfons@yahoo.com>
2019-04-17 16:57:45 -0400

    test email

--- test-mail.php
+++ test-mail.php
@@ -1,6 +1,8 @@
 <!-- PHP FOR FORMS -->
 
 <?php
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
     $first-name = $_POST['first name'];
     $last-name = $_POST['last name'];
     $email = $_POST['email'];
@@ -11,4 +13,6 @@
     $subject = 'Contact form danielcolangelo.com';
 
     $body = "From: $firstname $lastname\n E-Mail: $email\n Work: $work\n Message:\n $message";
+
+    echo "The email message was sent.";
 ?>
